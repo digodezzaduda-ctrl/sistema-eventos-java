f1a2796e6c71d6b06745fb167cd91de141157807
digodezzaduda-ctrl <digo.dezza.duda@gmail.com>
2025-09-09 10:52:37 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String cidade;
+    private List<Evento> eventosConfirmados;
+
+    public Usuario(String nome, String email, String cidade) {
+        this.nome = nome;
+        this.email = email;
+        this.cidade = cidade;
+        this.eventosConfirmados = new ArrayList<>();
+    }
+
+    public void confirmarEvento(Evento evento) {
+        eventosConfirmados.add(evento);
+        evento.adicionarParticipante(this);
+    }
+
+    public void cancelarEvento(Evento evento) {
+        eventosConfirmados.remove(evento);
+        evento.removerParticipante(this);
+    }
+
+    public List<Evento> getEventosConfirmados() {
+        return eventosConfirmados;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
